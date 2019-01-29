3b80ddfac14dd30f810e8a4b2763f82c9ead8d60
Norbiox <norberto9411@gmail.com>
2019-01-29 09:11:47 +0000

    scripts has been fixed!

--- piwificonnector/scripts/connect.sh
+++ piwificonnector/scripts/connect.sh
@@ -3,9 +3,13 @@
 WPA_CONFIG_FILE=$1
 WPA_STATUS_FILE=$2
 
+echo "Stopping Hotspot Mode ..."
+sudo systemctl stop hostapd.service
 sudo ifconfig wlan0 up
 sudo ip addr flush dev wlan0
 sudo truncate -s 0 $WPA_STATUS_FILE
+
+echo "Trying to connect with wi-fi network ..."
 sudo wpa_supplicant -B \
     -D wext \
     -i wlan0 \
@@ -15,13 +19,13 @@ sudo wpa_supplicant -B \
 declare -i i=0
 declare -i timeout=15
 
-echo "Trying to connect with wi-fi network ..."
 while [ $i -le $timeout ]; do
     if grep -iq 'CTRL-EVENT-CONNECTED' $WPA_STATUS_FILE; then
         echo "Device has been connected! Obtaining IP address ..."
         sudo dhclient wlan0
         exit 0
     elif grep -iq '4-Way Handshake failed' $WPA_STATUS_FILE; then
+        echo "Device couldn't connect with network"
         exit 1
     fi
 
