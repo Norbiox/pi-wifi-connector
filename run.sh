b25fa0022e243a1f9ef73c567ebaf0068ab6d9e9
Norbiox <norberto9411@gmail.com>
2019-02-05 22:48:40 +0100

    update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-export SECRET_KEY=thats-my-s3cr3tly-keeped-key
 export FLASK_ENV=app.py
-export FLASK_ENV=development
+export FLASK_ENV=production
 
 python3 -m flask run --host=0.0.0.0 --port=8080 --no-reload
