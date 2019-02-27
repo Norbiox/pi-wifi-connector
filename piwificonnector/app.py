bc2471138f0f8a2e32382805d7a5a51137872088
Norbiox <norberto9411@gmail.com>
2019-02-27 11:34:32 +0100

    added login page and decorators

--- piwificonnector/app.py
+++ piwificonnector/app.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
 import os
 from time import sleep
+from functools import wraps
 
-from flask import flash, Flask, redirect, render_template, request, url_for
+from flask import (flash, Flask, make_response, redirect, render_template,
+                   request, session, url_for)
 
 from .lib import WifiConnector
+
+
+class AuthenticationError(Exception):
+    pass
 
 
 app = Flask(__name__)
@@ -18,7 +24,33 @@ connector = WifiConnector(
 )
 
 
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        try:
+            logged_in = session['logged_in']
+            if logged_in is not True:
+                raise AuthenticationError('User not logged in')
+        except (KeyError, AuthenticationError) as e:
+            return redirect(url_for('login'))
+        else:
+            return f(*args, **kwargs)
+    return decorated_function
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        response = make_response(redirect(url_for('panel')))
+        if request.form['password'] == app.config['HOTSPOT_PASSWORD']:
+            session['logged_in'] = True
+        return response
+    else:
+        return render_template('login.html')
+
+
 @app.route('/', methods=['GET', 'POST'])
+@login_required
 def panel():
     if request.method == 'POST':
         ssid, key = request.form['ssid'], request.form['password']
@@ -28,17 +60,19 @@ def panel():
         except Exception as e:
             print(e)
             connector.disconnect_wifi()
-    return render_template('panel.html',
+    return render_template('manage.html',
                            networks=connector.get_available_networks())
 
 
 @app.route('/dc')
+@login_required
 def disconnect_wifi():
     connector.disconnect_wifi(remove_saved_credentials=True)
     return redirect(url_for('panel'))
 
 
 @app.route('/health')
+@login_required
 def health_check():
     if connector.is_online():
         flash("Device is online!")
--- piwificonnector/config.py
+++ piwificonnector/config.py
@@ -1,3 +1,14 @@
 SECRET_KEY = 'super-duper-not-hackable-at-all-key'
 WPA_CONFIG_FILE = '/home/pi/wifi_credentials.conf'
 WPA_STATUS_FILE = '/home/pi/wpa_logs.txt'
+
+hostapd_file = '/etc/hostapd/hostapd.conf'
+with open(hostapd_file) as f:
+    for line in f.readlines():
+        m = re.match(r'(?:wpa_passphrase=)(.+)', line)
+        if m:
+            HOTSPOT_PASSWORD = m.group(1)
+            break
+    else:
+        raise ValueError("Couldn't find hotspot password in {}".format(
+                         hostapd_file))
